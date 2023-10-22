cdbaf6ee930bab7a981ba4214c002f91002c2a94
Ömer Şükrü Uyduran <uyduranomersukru@gmail.com>
2023-10-22 19:31:51 +0300

    data source scripts are done and models arranged accordingly, closes #54

--- autoremover/database/models.py
+++ autoremover/database/models.py
@@ -36,7 +36,7 @@ class VehicleCategory(models.Model):
         return self.name
 
 class VehicleBrand(models.Model):
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=40, unique=True)
 
     def __str__(self):
         return self.name
@@ -48,7 +48,7 @@ class VehicleModel(models.Model):
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['brand', 'name'], name='vehiclemodel_brand_name_unique_constraint')
+            models.UniqueConstraint(fields=['brand', 'name', 'category'], name='vehiclemodel_brand_name_category_unique_constraint')
         ]
 
     def __str__(self):
@@ -57,6 +57,7 @@ class VehicleModel(models.Model):
 class VehicleYear(models.Model):
     model = models.ForeignKey(VehicleModel, on_delete=models.CASCADE)
     year = models.PositiveIntegerField()
+
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['model', 'year'], name='vehicleyear_model_year_constraint')
@@ -68,11 +69,19 @@ class VehicleYear(models.Model):
 class VehicleVersion(models.Model):
     fuel_type_choices = [
         ("P", "Petrol"),
-        ("D", "Diesel")
+        ("D", "Diesel"),
+        ("EL", "Electric"),
+        ("L", "LPG"),
+        ("E", "Ethanol"),
+        ("C", "CNG"),
+        ("PL", "Petrol/LPG"),
+        ("PE", "Petrol/Ethanol"),
+        ("PC", "Petrol/CNG"),
+        ("H", "Hybrid"),
     ]
     
     name = models.CharField(max_length=60)
-    fuel_type = models.CharField(max_length=1, choices=fuel_type_choices)
+    fuel_type = models.CharField(max_length=2, choices=fuel_type_choices)
 
     class Meta:
         constraints = [
