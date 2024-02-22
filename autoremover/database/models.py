442e45ee771d5491810cb5b51306b68068d1c1f4
Simurgan <uyduranomersukru@gmail.com>
2024-02-22 19:59:28 +0300

    create autodata project

--- autoremover/database/models.py
+++ autoremover/database/models.py
@@ -37,147 +37,11 @@ def create_transaction(sender, instance, **kwargs):
     instance.user.is_staff = True
     instance.user.save()
 
-class VehicleCategory(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-
-    def __str__(self):
-        return self.name
-
-class VehicleBrand(models.Model):
-    name = models.CharField(max_length=40, unique=True)
-
-    def __str__(self):
-        return self.name
-    
-class VehicleModel(models.Model):
-    name = models.CharField(max_length=40)
-    brand = models.ForeignKey(VehicleBrand, on_delete=models.CASCADE)
-    category = models.ForeignKey(VehicleCategory, on_delete=models.PROTECT)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['brand', 'name', 'category'], name='vehiclemodel_brand_name_category_unique_constraint')
-        ]
-
-    def __str__(self):
-        return str(self.brand) + " " + self.name
-    
-class VehicleYear(models.Model):
-    model = models.ForeignKey(VehicleModel, on_delete=models.CASCADE)
-    year = models.PositiveIntegerField()
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['model', 'year'], name='vehicleyear_model_year_constraint')
-        ]
-
-    def __str__(self):
-        return str(self.model) + " " + str(self.year)
-    
-class VehicleVersion(models.Model):
-    fuel_type_choices = [
-        ("P", "Petrol"),
-        ("D", "Diesel"),
-        ("EL", "Electric"),
-        ("L", "LPG"),
-        ("E", "Ethanol"),
-        ("C", "CNG"),
-        ("PL", "Petrol/LPG"),
-        ("PE", "Petrol/Ethanol"),
-        ("PC", "Petrol/CNG"),
-        ("H", "Hybrid"),
-    ]
-    
-    name = models.CharField(max_length=60)
-    fuel_type = models.CharField(max_length=2, choices=fuel_type_choices)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['name', 'fuel_type'], name='vehicleversion_name_fueltype_unique_constraint')
-        ]
-
-    def __str__(self):
-        for ftc in self.fuel_type_choices:
-            if ftc[0] == self.fuel_type:
-                fuel_type = ftc[1]
-
-        return self.name + " - " + fuel_type
-    
-class EcuBrand(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-
-    def __str__(self):
-        return self.name
-    
-class EcuModel(models.Model):
-    name = models.CharField(max_length=30)
-    brand = models.ForeignKey(EcuBrand, on_delete=models.CASCADE)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['name', 'brand'], name='ecumodel_name_brand_unique_constraint')
-        ]
-
-    def __str__(self):
-        return str(self.brand) + " " + self.name
-    
-class Vehicle(models.Model):
-    vehicle_year = models.ForeignKey(VehicleYear, on_delete=models.CASCADE)
-    version = models.ForeignKey(VehicleVersion, on_delete=models.PROTECT)
-    ecu_model = models.ForeignKey(EcuModel, on_delete=models.PROTECT)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['vehicle_year', 'version', 'ecu_model'], name='vehicle_vehicleyear_version_ecumodel_unique_constraint')
-        ]
-    
-    @classmethod
-    def get_version_option_list(cls, **kwargs):
-        ret_list = []
-        for instance in cls.objects.filter(kwargs):
-            if instance.version not in ret_list:
-                ret_list.append(instance.version)
-
-        return ret_list
-    
-    @classmethod
-    def get_ecu_model_option_list(cls, **kwargs):
-        ret_list = []
-        for instance in cls.objects.filter(kwargs):
-            if instance.version not in ret_list:
-                ret_list.append(instance.ecu_model)
-                
-        return ret_list
-    
-    def __str__(self):
-        return str(self.vehicle_year) + " - " + str(self.version) + " - " + str(self.ecu_model)  
-
 class ConnectionTool(models.Model):
     name = models.CharField(max_length=30, unique=True)
     
     def __str__(self):
         return self.name
-    
-class VehiclePotential(models.Model):
-    vehicle = models.OneToOneField(Vehicle, on_delete=models.CASCADE)
-    old_hp = models.PositiveIntegerField()
-    new_hp = models.PositiveIntegerField()
-    old_nm = models.PositiveIntegerField()
-    new_nm = models.PositiveIntegerField()
-    known_reading_methods = models.ManyToManyField(ConnectionTool)
-
-class Ecu(models.Model):
-    model = models.ForeignKey(EcuModel, on_delete=models.CASCADE)
-    number = models.CharField(max_length=10)
-    carmanufacturers = models.CharField(max_length=130, null=True, blank=True)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['model', 'number'], name='ecu_model_number_unique_constraint')
-        ]
-    
-    def __str__(self):
-        return self.number
 
 class FileProcess(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -186,11 +50,12 @@ class FileProcess(models.Model):
         return self.name
 
 class ProcessPricing(models.Model):
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    vehicle = models.PositiveBigIntegerField()
     process = models.ForeignKey(FileProcess, on_delete=models.CASCADE)
-    master_price = models.FloatField()
-    slave_price = models.FloatField()
-    euro_price = models.FloatField()
+    master_try_price = models.FloatField()
+    slave_try_price = models.FloatField()
+    master_eur_price = models.FloatField()
+    slave_eur_price = models.FloatField()
 
     class Meta:
         constraints = [
@@ -198,7 +63,7 @@ class ProcessPricing(models.Model):
         ]
 
 class FileRequest(models.Model):
-    transmissin_choices = [
+    transmission_choices = [
         ("A", "AUTO"),
         ("M", "MANUAL")
     ]
@@ -223,12 +88,12 @@ class FileRequest(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.PROTECT)
+    vehicle = models.PositiveBigIntegerField()
     tool = models.ForeignKey(ConnectionTool, on_delete=models.PROTECT)
     processes = models.ManyToManyField(FileProcess)
 
     file_type = models.CharField(max_length=1, choices=file_type_choices)
-    transmission = models.CharField(max_length=1, choices=transmissin_choices)
+    transmission = models.CharField(max_length=1, choices=transmission_choices)
     tool_type = models.CharField(max_length=1, choices=tool_type_choices)
 
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -591,6 +456,8 @@ class SystemSetting(models.Model):
     bank_account_owner_name = models.CharField(max_length=100)
     credit_try_price = models.FloatField(default=1)
     credit_eur_price = models.FloatField(default=1)
+    vehicle_data_backend_ip = models.GenericIPAddressField()
+    vehicle_data_backend_port = models.PositiveIntegerField()
 
     def __str__(self):
         return "System settings"
