07674544986b01745ddd7cc86c83b92e8a5d042d
Simurgan <uyduranomersukru@gmail.com>
2024-03-09 16:44:46 +0300

    implement vehicle_data_api in autodata but not tested

--- autodata/api/views.py
+++ autodata/api/views.py
@@ -6,121 +6,141 @@ from urllib.parse import unquote
 from django.core.paginator import Paginator
 from django.db.models import Q
 
+from functools import reduce
+import operator
 import json
 
 from database.models import *
 
 # Create your views here.
 
-def vehicle_select_api(request):
+def get_vehicle_queries(filters):
+    vehicle_filters = json.loads(filters)
+    q_list = []
+    for key, value in vehicle_filters.items():
+        if key == 'vehicle_category_ids':
+            q_list.append(Q(vehicle_year__model__category_id__in=value))
+
+        elif key == 'vehicle_brand_ids':
+            q_list.append(Q(vehicle_year__model__brand_id__in=value))
+
+        elif key == 'vehicle_model_ids':
+            q_list.append(Q(vehicle_year__model_id__in=value))
+
+        elif key == 'vehicle_year_ids':
+            q_list.append(Q(vehicle_year_id__in=value))
+        
+        elif key == 'vehicle_version_ids':
+            q_list.append(Q(version_id__in=value))
+
+        elif key == 'ecu_model_ids':
+            q_list.append(Q(ecu_model_id__in=value))
+
+    return q_list
+
+def vehicle_data_api(request):
     params = request.GET
 
-    requested = params.get('requested')
+    requests = json.loads(params.get('requests'))
 
-    if requested == 'vehicle_category':
-        vehicle_categories = VehicleCategory.objects.all().order_by('name')
-        data_type = 'vehicle category'
-        data = serialize("json", vehicle_categories, fields=['id', 'name'])
-     
-    elif requested == 'vehicle_brand':
-        category_id = int(params.get('vehicle_category'))
-        vehicle_model_ids = VehicleModel.objects.filter(category_id=category_id).values('brand_id')
-        vehicle_brands = VehicleBrand.objects.filter(id__in=vehicle_model_ids).order_by('name')
-        data_type = 'vehicle brand'
-        data = serialize("json", vehicle_brands, fields=['id', 'name'])
+    data = {}
+    for requested in requests:
+        if requested == 'vehicle_category':
+            vehicle_categories = VehicleCategory.objects.all().order_by('name')
+            data["vehicle_category"] = serialize("json", vehicle_categories, fields=['id', 'name'])
+        
+        elif requested == 'vehicle_brand':
+            category_ids = json.loads(params.get('vehicle_categories'))
+            vehicle_model_ids = VehicleModel.objects.filter(category_id__in=category_ids).values('brand_id')
+            vehicle_brands = VehicleBrand.objects.filter(id__in=vehicle_model_ids).order_by('name')
+            data["vehicle_brand"] = serialize("json", vehicle_brands, fields=['id', 'name'])
 
-    elif requested == 'vehicle_model':
-        category_id = int(params.get('vehicle_category'))
-        brand_id = int(params.get('vehicle_brand'))
-        vehicle_models = VehicleModel.objects.filter(category_id=category_id, brand_id=brand_id).order_by('name')
-        data_type = 'vehicle model'
-        data = serialize("json", vehicle_models, fields=['id', 'name'])
-    
-    elif requested == 'vehicle_year':
-        model_id = int(params.get('vehicle_model'))
-        years = VehicleYear.objects.filter(model_id=model_id).order_by('year')
-        data_type = 'vehicle year'
-        data = serialize("json", years, fields=['id', 'year'])
+        elif requested == 'vehicle_model':
+            category_ids = json.loads(params.get('vehicle_categories'))
+            brand_ids = json.loads(params.get('vehicle_brands'))
+            vehicle_models = VehicleModel.objects.filter(category_id__in=category_ids, brand_id__in=brand_ids).order_by('name')
+            data["vehicle_model"] = serialize("json", vehicle_models, fields=['id', 'name'])
+        
+        elif requested == 'vehicle_year':
+            model_ids = json.loads(params.get('vehicle_models'))
+            years = VehicleYear.objects.filter(model_id__in=model_ids).order_by('year')
+            data["vehicle_year"] = serialize("json", years, fields=['id', 'year'])
 
-    elif requested == 'vehicle_version':
-        vehicle_year_id = int(params.get('vehicle_year'))
-        vehicle_version_ids = Vehicle.objects.filter(vehicle_year_id=vehicle_year_id).values('version_id')
-        engines = VehicleVersion.objects.filter(id__in=vehicle_version_ids).order_by('name', 'fuel_type')
-        data_type = 'vehicle version'
-        data = serialize("json", engines, fields=['id', 'name', 'fuel_type'])
+        elif requested == 'vehicle_version':
+            vehicle_year_ids = json.loads(params.get('vehicle_years'))
+            vehicle_version_ids = Vehicle.objects.filter(vehicle_year_id__in=vehicle_year_ids).values('version_id')
+            engines = VehicleVersion.objects.filter(id__in=vehicle_version_ids).order_by('name', 'fuel_type')
+            data["vehicle_version"] = serialize("json", engines, fields=['id', 'name', 'fuel_type'])
 
-    elif requested == 'ecu_type':
-        vehicle_year_id = int(params.get('vehicle_year'))
-        version_id = int(params.get('vehicle_version'))
-        ecu_model_ids = Vehicle.objects.filter(vehicle_year_id=vehicle_year_id, version_id=version_id).values('ecu_model_id')
-        ecu_models = EcuModel.objects.filter(id__in=ecu_model_ids).order_by('brand__name', 'name')
-        data_type = 'ecu type'
-        data = serialize("json", ecu_models, fields=['id', 'name'])
+        elif requested == 'ecu_type':
+            vehicle_year_ids = json.loads(params.get('vehicle_years'))
+            version_ids = json.loads(params.get('vehicle_versions'))
+            ecu_model_ids = Vehicle.objects.filter(vehicle_year_id__in=vehicle_year_ids, version_id__in=version_ids).values('ecu_model_id')
+            ecu_models = EcuModel.objects.filter(id__in=ecu_model_ids).order_by('brand__name', 'name')
+            data["ecu_type"] = serialize("json", ecu_models, fields=['id', 'name'])
 
-    elif requested == 'vehicle':
-        vehicle_year_id = int(params.get("vehicle_year_id"))
-        version_id = int(params.get("vehicle_version_id"))
-        ecu_model_id = int(params.get("ecu_model_id"))
+        elif requested == 'vehicle':
+            vehicle_filters = params.get('vehicle_filters')
+            if vehicle_filters is None:
+                vehicles = Vehicle.objects.all()
+            else:
+                queries = get_vehicle_queries(vehicle_filters)
+                vehicles = Vehicle.objects.filter(reduce(operator.and_, queries))
 
-        vehicle = Vehicle.objects.get(vehicle_year_id=vehicle_year_id, version_id=version_id, ecu_model_id=ecu_model_id)
-        data_type = 'vehicle'
-        data = {'id': vehicle.id}
+            page = params.get('vehicle_page')
+            if page is not None:
+                pagenum = int(page)
+
+                paginator = Paginator(vehicles, 10)
+                page = paginator.page(int(pagenum))
+                start_page = max(1, page.number - 5)
+                end_page = min(paginator.num_pages, max(page.number + 5, 10))
+                page_list = list(range(start_page, end_page + 1))
+
+                vehicle_data = []
+                for vehicle in page.object_list:
+                    vehicle_data.append({
+                        'id': vehicle.id,
+                        'vehicle_category': vehicle.vehicle_year.model.category.name,
+                        'vehicle_brand': vehicle.vehicle_year.model.brand.name,
+                        'vehicle_model': vehicle.vehicle_year.model.name,
+                        'vehicle_year': vehicle.vehicle_year.year,
+                        'vehicle_version': vehicle.version.name,
+                        'ecu_model': vehicle.ecu_model.name
+                    })
+
+                data["vehicle"] = {
+                    'data_amount': paginator.count,
+                    'start_index': page.start_index(),
+                    'end_index': page.end_index(),
+                    'current_page': page.number,
+                    'previous_page_disabled': not page.has_previous(),
+                    'following_page_disabled': not page.has_next(),
+                    'page_list': page_list,
+                    'data': vehicle_data
+                }
+            else:
+                vehicle_data = []
+                for vehicle in vehicles:
+                    vehicle_data.append({
+                        'id': vehicle.id,
+                        'vehicle_category': vehicle.vehicle_year.model.category.name,
+                        'vehicle_brand': vehicle.vehicle_year.model.brand.name,
+                        'vehicle_model': vehicle.vehicle_year.model.name,
+                        'vehicle_year': vehicle.vehicle_year.year,
+                        'vehicle_version': vehicle.version.name,
+                        'ecu_model': vehicle.ecu_model.name
+                    })
+                data["vehicle"] = {
+                    'data_amount': vehicles.count(),
+                    'data': vehicle_data
+                }
 
     context = {
-        'data_type': data_type,
         'data': data
     }
 
     return JsonResponse(context)
-
-def vehicle_filter_api(request):
-    params = request.GET
-    filters = json.loads(params.get('filters'))
-    print(filters)
-
-    q_list = []
-    for key, value in filters.items():
-        if key == 'vehicle_category_id':
-            # value = int(value)
-            print("category_ids:")
-            print(value)
-            q_list.append(Q(vehicle_year__model__category_id__in=value))
-
-        elif key == 'vehicle_brand_id':
-            # value = int(value)
-            print("brand_ids:")
-            print(value)
-            q_list.append(Q(vehicle_year__model__brand_id__in=value))
-
-        elif key == 'vehicle_model_id':
-            # value = int(value)
-            print("model_ids:")
-            print(value)
-            q_list.append(Q(vehicle_year__model_id__in=value))
-
-        elif key == 'vehicle_year_id':
-            # value = int(value)
-            print("year_ids:")
-            print(value)
-            q_list.append(Q(vehicle_year_id__in=value))
-        
-        elif key == 'vehicle_version_id':
-            # value = int(value)
-            print("version_ids:")
-            print(value)
-            q_list.append(Q(version_id__in=value))
-
-        elif key == 'ecu_model_id':
-            # value = int(value)
-            print("ecu_model_ids:")
-            print(value)
-            q_list.append(Q(ecu_model_id__in=value))
-
-    # vehicle_year = params.get("vehicle_year")
-    # version = params.get("vehicle_version")
-    # ecu_model = params.get("ecu_model")
-
-    # vehicles = Vehicle.objects.filter(vehicle_year_id=vehicle_year_id, version_id=version_id, ecu_model_id=ecu_model_id).order_by('id')
 
 def dtc_search_api(request):
     params = request.GET
